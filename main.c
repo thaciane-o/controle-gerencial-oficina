0620b43d25398652bbc395cd2697173472b30836
Thaciane <thacianeoliveira0207@gmail.com>
2024-11-15 17:06:08 -0300

    Correção da main

--- main.c
+++ main.c
@@ -28,33 +28,6 @@ int main() {
         scanf("%d", &opcaoArmazenamento);
     } while (opcaoArmazenamento < 1 || opcaoArmazenamento > 3);
 
-
-
-    do {
-        printf("\n=================================\n"
-            "|         MENU PRINCIPAL        |\n"
-            "=================================\n"
-            "|  1  | Oficina                 |\n"
-            "|  2  | Clientes                |\n"
-            "|  3  | Veículos               |\n"
-            "|  4  | Peças                  |\n"
-            "|  5  | Fornecedores            |\n"
-            "|  6  | Serviços               |\n"
-            "|  7  | Funcionários           |\n"
-            "|  8  | Sair                    |\n"
-            "=================================\n"
-        "Escolha uma opção:\n");
-        scanf("%d", &opcaoMenu);
-
-    do {
-        printf("Como deseja armazenar?\n"
-           "1. Texto\n"
-           "2. Binário\n"
-           "3. Memória\n"
-            "Escolha uma opção: \n");
-        scanf("%d", &opcaoArmazenamento);
-    } while (opcaoArmazenamento < 1 || opcaoArmazenamento > 3);
-
     do {
         printf("\n=================================\n"
             "|         MENU PRINCIPAL        |\n"
